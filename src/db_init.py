0bb8b44deef84e0879792498063751766c6ddd15
Denis Gorodetskiy <denez.gorodetskii@gmail.com>
2025-02-12 12:20:58 +0300

    develop - db_init.py was fixed

--- src/db_init.py
+++ src/db_init.py
@@ -1,14 +1,15 @@
 import datetime
-import enum
 
 from typing import Literal
 
-from sqlalchemy import DateTime, Integer, String, Enum, JSON
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy import DateTime, Integer, String, Enum, ForeignKey
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.sql import func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-OrderStatus = Literal["created", "delivering", "finished", "cancelled"]
+
+OrderStatus = Literal["created", "finished", "cancelled"]
+DataAvailable = Literal["yes", "no", "deleted"]
 
 
 class Base(DeclarativeBase): ...
@@ -18,45 +19,91 @@ class UserModel(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=False)
-    tg_username: Mapped[str] = mapped_column(String, nullable=True)
+    username: Mapped[str] = mapped_column(String, nullable=True)
+
 
 class OrderDataModel(Base):
     __tablename__ = "order_data"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    # 2 table setup
-    # user_id ForeignKey
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("UserModel.id"))
 
-    # 1 table setup
-    # tg_id
-    # tg_username
-    
-    # Problem order configs
-    # Solution 1 Literal = bad scalability
-    # Solution 2 DB entry/JSON
-    # Solution 3 store order configuration as is
-    #   order_config: Mapped[str] = json.dumps({'type': 'a', ...})
-    
-    type: Mapped[str]
-    shape: Mapped[str]
-    filling: Mapped[str]
-    confit: Mapped[str]
-    
-    comment: Mapped[str]
+    type_id: Mapped[int] = mapped_column(String, ForeignKey("TypeDataModel.id"))
+    shape_id: Mapped[int] = mapped_column(
+        String, ForeignKey("ShapeDataModel.id"), nullable=True
+    )
+    flavour_id: Mapped[int] = mapped_column(String, ForeignKey("FlavourDataModel.id"))
+    confit_id: Mapped[int] = mapped_column(
+        String, ForeignKey("ConfitDataModel.id"), nullable=True
+    )
+
+    comment: Mapped[str] = mapped_column(String, nullable=True)
 
     status: Mapped[OrderStatus] = mapped_column(
-        Enum(
-            "created", "delivering", "finished", "cancelled",
-            name="order_status_enum"
-        ),
-        nullable=False
+        Enum("created", "finished", "cancelled", name="order_status_enum"),
+        nullable=False,
     )
-    
-    created: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+    created: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+
+
+class TypeDataModel(Base):
+    __tablename__ = "type_data"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    type: Mapped[str] = mapped_column(String, nullable=False)
+    disc: Mapped[str] = mapped_column(String, nullable=False)
+
+    available: Mapped[DataAvailable] = mapped_column(
+        Enum("yes", "no", "deleted", name="data_available_enum"), nullable=False
+    )
+
+
+class ShapeDataModel(Base):
+    __tablename__ = "shape_data"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    shape: Mapped[str] = mapped_column(String, nullable=False)
+    disc: Mapped[str] = mapped_column(String, nullable=False)
+
+    available: Mapped[DataAvailable] = mapped_column(
+        Enum("yes", "no", "deleted", name="data_available_enum"), nullable=False
+    )
+
+
+class FlavourDataModel(Base):
+    __tablename__ = "flavour_data"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    flavour: Mapped[str] = mapped_column(String, nullable=False)
+    disc: Mapped[str] = mapped_column(String, nullable=False)
+
+    available: Mapped[DataAvailable] = mapped_column(
+        Enum("yes", "no", "deleted", name="data_available_enum"), nullable=False
+    )
+
+
+class ConfitDataModel(Base):
+    __tablename__ = "confit_data"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    confit: Mapped[str] = mapped_column(String, nullable=False)
+    disc: Mapped[str] = mapped_column(String, nullable=False)
+
+    available: Mapped[DataAvailable] = mapped_column(
+        Enum("yes", "no", "deleted", name="data_available_enum"), nullable=False
+    )
+
 
 class DBInit:
-    def __init__(self, bd_name: str):
-        self.engine = create_async_engine(f"sqlite+aiosqlite:///{bd_name}.db")
+    def __init__(self, db_name: str):
+        self.engine = create_async_engine(f"sqlite+aiosqlite:///{db_name}.db")
         self.session = async_sessionmaker(self.engine, expire_on_commit=False)
 
     async def setup_database(self):
